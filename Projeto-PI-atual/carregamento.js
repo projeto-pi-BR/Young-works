4688b33f0ac90729bc19f68437a8e8733f40ebf8
mickeas_willams <128330949+mickeas@users.noreply.github.com>
2025-05-04 21:17:09 -0300

    Add files via upload

---
+++ Projeto-PI-atual/carregamento.js
@@ -0,0 +1,20 @@
+// let menu = document.querySelector('#menu-icon');
+// let navlist = document.querySelector('.navlist');
+
+// menu.onclick = () => {
+//     menu.classList. toggle('bx-x');
+//     navlist.classList. toggle('open');
+// };
+
+// const sr = scrollreveal ({
+//     distance: '65px',
+//     duration: 2600,
+//     delay: 450,
+//     reset: true
+// });
+
+// sr.reveal('.hero-text',{delay:200, origin: 'top'});
+
+setTimeout(function() {
+    window.location.replace("/pages/apresentacao.html");
+  }, 2500);
